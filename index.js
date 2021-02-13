762cefbb706d9776e5263420070ac3837d909ef7
Hercules Merscher <hlmerscher@gmail.com>
2021-02-13 18:43:42 +0100

    more terms to query

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { GiphyFetch } from '@giphy/js-fetch-api'
 import { renderGrid } from '@giphy/js-components'
 
 const gf = new GiphyFetch(process.env.GIPHY_API_KEY)
-const fetchGifs = (offset) => gf.search('lgtm')
+const fetchGifs = (offset) => gf.search('lgtm, approved, celebrate, thumbsup')
 const getWidth = () => window.innerWidth;
 
 window.onload = () => {
