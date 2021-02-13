a6b68bbe2431c4b871e043e1428762e63a595228
Hercules Merscher <hlmerscher@gmail.com>
2021-02-13 17:22:32 +0100

    lgtm gifs website

---
+++ index.js
@@ -0,0 +1,19 @@
+import { GiphyFetch } from '@giphy/js-fetch-api'
+import { renderGrid } from '@giphy/js-components'
+
+const gf = new GiphyFetch(process.env.GIPHY_API_KEY)
+const fetchGifs = (offset) => gf.search('lgtm')
+const getWidth = () => window.innerWidth;
+
+window.onload = () => {
+    const main = document.getElementById('main')
+    const width = getWidth()
+    renderGrid({
+        width,
+        fetchGifs,
+        columns: width < 500 ? 2 : 4,
+        gutter: 6,
+      },
+      main
+    )
+}
